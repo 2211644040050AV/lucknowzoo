4259af0de82e4ee14efa38b9e9b9fab9179a74f8
Ayush Vishwakarma <bca2211644040050.av@goel.edu.in>
2025-01-21 22:42:14 +0530

    Update the previous day code and making a route to navigate the different path of the website

--- src/App.js
+++ src/App.js
@@ -1,45 +1,44 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Banner from "./GlobalComponents/Banner/Banner";
-import MsgText from "./GlobalComponents/MsgText/MsgText";
-import NavigationMenu from "./GlobalComponents/NavigationMenu/NavigationMenu";
-import TopButton from "./GlobalComponents/TopButton/TopButton";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Banner from "../src/GlobalComponents/Banner/Banner";
+import MsgText from "../src/GlobalComponents/MsgText/MsgText";
+import NavigationMenu from "../src/GlobalComponents/NavigationMenu/NavigationMenu";
 import Home from "./Components/Home/Home";
-import NewsAndUpdate from "./GlobalComponents/NewsAndUpdate/NewsAndUpdate";
-
+// import AboutUs from './Components/AboutUs/AboutUs';
+// import History from './Components/History/History';
+// import Animals from './Components/Animals/Animals';
+// import Reptiles from './Components/Reptiles/Reptiles';
+// import PhotoGallery from './Components/PhotoGallery/PhotoGallery';
+// import ContactUs from './Components/ContactUs/ContactUs';
+// import RulesAndRegulations from './Components/RulesAndRegulations/RulesAndRegulations';
+// import EntryFee from './Components/EntryFee/EntryFee';
+// import VisitingHours from './Components/VisitingHours/VisitingHours';
+// import BalTrain from './Components/BalTrain/BalTrain';
 
 function App() {
   return (
-    <>
-      <div className="Container">
+    <Router>
+      <div className="container-main">
         <div className="child">
-          <div className="row">
-            <div className="col-12">
-              <Banner />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-12">
-              <MsgText />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-12">
-              <NavigationMenu />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-8">
-              <Home />
-            </div>
-            <div className="col-4">
-              <NewsAndUpdate />
-            </div>
-          </div>
+          <Banner /> 
+          <MsgText />
+          <NavigationMenu />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/history" element={<History />} />
+        <Route path="/animals" element={<Animals />} />
+        <Route path="/reptiles" element={<Reptiles />} />
+        <Route path="/photo-gallery" element={<PhotoGallery />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/rules-and-regulations" element={<RulesAndRegulations />} />
+        <Route path="/entry-fee" element={<EntryFee />} />
+        <Route path="/visiting-hours" element={<VisitingHours />} />
+        <Route path="/bal-train" element={<BalTrain />} /> */}
+          </Routes>
         </div>
-        <TopButton />
       </div>
-    </>
+    </Router>
   );
 }
 
--- src/GlobalComponents/NavigationMenu/NavigationMenu.js
+++ src/GlobalComponents/NavigationMenu/NavigationMenu.js
@@ -1,28 +1,33 @@
-import React, { useState } from "react";
-import { menuItems } from "../../utils/menuItems"; // Importing the menu items array
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { menuItems } from '../../utils/menuItems';
 
 function NavigationMenu() {
-  const [active, setActive] = useState(null);
-
   return (
-    <>
-      <div className="navigation-menu">
-        {menuItems.map((item, index) => {
-          return (
-            <React.Fragment key={item.id}>
-              <a
-                className={`items ${active === item.id ? "active" : ""}`}
-                href={item.link}
-                onClick={() => setActive(item.id)} // Set active item on click
-              >
-                {item.title}
-              </a>
-              {index < menuItems.length - 1 && " | "} {/* Add pipe between links */}
-            </React.Fragment>
-          );
-        })}
-      </div>
-    </>
+    <div className="navigation-menu">
+      {menuItems.map((item, index) => (
+        <React.Fragment key={item.id}>
+          <NavLink
+            to={item.link}
+            className="items"
+            style={({ isActive }) =>
+              isActive
+                ? {
+                    color: 'rgb(58, 170, 84)',
+                    fontWeight: 'bold',
+                    padding: '5px 10px',
+                    borderRadius: '5px',
+                  }
+                : undefined
+            }
+          >
+            {item.title}
+          </NavLink>
+          {/* Add separator only if not the last item */}
+          {index < menuItems.length - 1 && <span> | </span>}
+        </React.Fragment>
+      ))}
+    </div>
   );
 }
 
